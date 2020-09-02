98cfeeb871d3f196c1b832cb26e18812614bef16
swanITworksl <wm_swan@icloud.com>
2020-09-02 21:40:07 +0200

    configured gatsby node js file for multilanguages sites

---
+++ config/i18n.js
@@ -0,0 +1,21 @@
+module.exports = {
+    en: {
+      default: true,
+      path: `en`,
+      locale: `en-UK`,
+      dateFormat: `DD/MM/YYYY`,
+      siteLanguage: `en`,
+      ogLanguage: `en_UK`,
+      defaultTitle: `Software house company from London`,
+      defaultDescription: `We at Swan IT Works know it can be tedious when cooperating gaining an agreement within the IT industry. Our diverse global team of business practitioners and specialists put your needs as our core focus. Yes, we’re a software house; however, we have diversified experience in production, services, hotel and trade management. All of us at Swan IT Works are happy to serve your business, whether it’s a small start-up or a large enterprise. Most of us are remote, although, if required we can come to you or meet in Central London. We work around you.`,
+    },
+    de: {
+      path: `de`,
+      locale: `de-DE`,
+      dateFormat: `DD.MM.YYYY`,
+      siteLanguage: `de`,
+      ogLanguage: `de_DE`,
+      defaultTitle: `Software-Hausfirma aus London`,
+      defaultDescription: `Wir bei Swan IT Works wissen, dass es mühsam sein kann, eine Vereinbarung innerhalb der IT-Branche zu treffen. Unser vielfältiges globales Team von Geschäftsleuten und Spezialisten stellt Ihre Bedürfnisse in den Mittelpunkt. Ja, wir sind ein Softwarehaus. Wir verfügen jedoch über vielfältige Erfahrungen in den Bereichen Produktion, Dienstleistungen, Hotel- und Handelsmanagement. Wir alle bei Swan IT Works freuen uns, Ihr Unternehmen zu bedienen, egal ob es sich um ein kleines Start-up oder ein großes Unternehmen handelt. Die meisten von uns sind abgelegen, aber bei Bedarf können wir zu Ihnen kommen oder uns in Central London treffen. Wir arbeiten um dich herum.`,
+    },
+  }
